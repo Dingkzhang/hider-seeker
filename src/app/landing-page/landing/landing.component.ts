753c252daa54d762b5f03197faa7233ce8c623d2
Dingkzhang <dingkzhang@utexas.edu>
2019-05-20 14:43:19 -0500

    updated landing page

--- src/app/landing-page/landing/landing.component.ts
+++ src/app/landing-page/landing/landing.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
+
+import { JsonConversionService } from '../../services/json-conversion-service/json-conversion.service';
+import { ParagraphConversionService } from '../../services/paragraph-conversion-service/paragraph-conversion.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +13,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  blogIndexSubscription: Subscription;
+  portfolioIndexSubscription: Subscription;
 
-  ngOnInit() {
+  blogIndex;
+  blogInfo;
+  latestBlogInfo;
+
+  portfolioIndex;
+  portfolioInfo;
+  latestPortfolioInfo;
+
+  constructor(
+    private http: HttpClient,
+    private jsonConversion: JsonConversionService,
+    private paragraphConversion: ParagraphConversionService) {
+    this.setupBlogIndex();
+    this.setupPorfolioIndex();
+
+    console.log(this.latestBlogInfo);
   }
+
+  private setupBlogIndex() {
+    this.blogIndexSubscription = this.getBlogIndex().subscribe(data => {
+      this.blogIndex = data;
+      this.blogInfo = this.paragraphConversion.convertParagraphArray(this.blogIndex.story_library)
+      const blogLength = this.blogInfo.length;
+      this.latestBlogInfo = this.blogInfo[blogLength - 1];
+      console.log(this.latestBlogInfo);
+    });
+  }
+
+  private getBlogIndex(): Observable<any> {
+    return this.http.get('../../../assets/blog-assets/index.json');
+  }
+
+  private setupPorfolioIndex() {
+    this.portfolioIndexSubscription = this.getPorfolioIndex().subscribe(data => {
+      this.portfolioIndex = data;
+      this.portfolioInfo = this.paragraphConversion.convertParagraphArray(this.portfolioIndex.project_library);
+      const portfolioLength = this.portfolioInfo.length;
+      this.latestPortfolioInfo = this.portfolioInfo[portfolioLength - 1];
+  
+      console.log(this.latestPortfolioInfo);
+
+    })
+  }
+
+  private getPorfolioIndex() {
+    return this.http.get('../../../assets/portfolio-assets/index.json');
+  }
+
+
+  ngOnInit() { }
+
+  ngOnDestroy() {
+    if (this.blogIndexSubscription && !this.blogIndexSubscription.closed) {
+      this.blogIndexSubscription.unsubscribe();
+    }
+
+    if (this.portfolioIndexSubscription && !this.portfolioIndexSubscription.closed) {
+      this.portfolioIndexSubscription.unsubscribe();
+    }
+  }
+
 
 }
